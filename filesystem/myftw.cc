e950512b00a8afe1cdf507af34903fa33935f936
BewareMyPower <xyzinfernity@163.com>
2018-03-21 05:30:52 -0700

    修复功能错误

--- filesystem/myftw.cc
+++ filesystem/myftw.cc
@@ -13,19 +13,21 @@ inline void show_file_cnt(long nfile, const char* description) {
 class PathBuffer {
 public:
     PathBuffer() = default;
-    PathBuffer(const char* s) { add_file(s); }
+    PathBuffer(const char* dirname);
     PathBuffer(const PathBuffer&) = delete;
 
     bool add_file(const char* name);  // 添加文件名到路径末尾，若添加失败则返回false
     void resize(size_t newlen);  // 改变路径大小为newlen
     bool is_dir() const;
     const char* fullname() const { return _str; }
-    const char* basename() const { return _basestr + 1; }
+    const char* basename() const { return _basestr; }
     size_t length() const { return _len; }
 
     mutable struct stat _stat;  // 路径对应的元数据
     bool lstat() const;  // 获取路径对应文件的元数据，若无法获取则返回false
 private:
+    void set_basestr();
+
     char _str[PATH_MAX] = {0};  // 实际路径字符串缓冲区
     const char* _basestr = _str;  // 指向文件名
     size_t _len = 0;  // 路径长度
@@ -53,7 +55,7 @@ void deal_with_path(const PathBuffer& path, int) {
 class FileTreeIterator {
 public:
     using CallBack = std::function<void (const PathBuffer&, int)>;
-    FileTreeIterator(const char* dirname, CallBack func);
+    FileTreeIterator(const char* dirname, CallBack func) : _path(dirname), _func(func) {}
 
     void dfs(int depth) {
         DIR* dp = opendir(_path.fullname());
@@ -66,6 +68,7 @@ public:
             if (strcmp(dirp->d_name, ".") == 0 ||
                 strcmp(dirp->d_name, "..") == 0)
                 continue;
+            
             size_t oldlen = _path.length();
             // 递归遍历子目录
             _path.add_file(dirp->d_name);
@@ -87,11 +90,12 @@ private:
 int main(int argc, char* argv[]) {
     if (argc != 2)
         err_quit("usage: %s <starting-pathname>", argv[0]);
+    FileTreeIterator tree(argv[1], deal_with_path);
+    tree.dfs(0);
+
     ntot = nreg + ndir + nblk + nchr + nfifo + nslink + nsock;
     if (ntot == 0)
         ntot = 1;  // 避免和0整除
-    FileTreeIterator tree(argv[1], deal_with_path);
-    tree.dfs(0);
     show_file_cnt(nreg, "regular files");
     show_file_cnt(ndir, "directories");
     show_file_cnt(nblk, "block special");
@@ -100,6 +104,14 @@ int main(int argc, char* argv[]) {
     show_file_cnt(nslink, "symbol links");
     show_file_cnt(nsock, "sockets");
     return 0;
+}
+
+PathBuffer::PathBuffer(const char* dirname) {
+    _len = strlen(dirname);
+    if (_len >= PATH_MAX)
+        err_quit("%s is too long", dirname);
+    strncpy(_str, dirname, _len);
+    _str[_len] = '\0';
 }
 
 inline bool PathBuffer::lstat() const {
@@ -113,14 +125,16 @@ inline bool PathBuffer::lstat() const {
 inline bool PathBuffer::add_file(const char* name) {
     if (!name)
         return false;
-    if (_str[_len] != '/' && name[0] != '/')
+    if (_str[_len] != '/' && name[0] != '/') {
         _str[_len++] = '/';
+        _str[_len] = '\0';
+    }
     size_t namelen = strlen(name);
     if (namelen + _len + 1 < PATH_MAX) {
         strncpy(_str + _len, name, namelen);
         _len += namelen;
         _str[_len] = '\0';
-        for (_basestr = _str + _len; *_basestr != '/'; --_basestr) {}
+        set_basestr();
         return true;
     } else {
         err_msg("Path is too long!\n  (%s%s)\n", _str, name);
@@ -132,16 +146,17 @@ inline void PathBuffer::resize(size_t newlen) {
     newlen = (newlen >= PATH_MAX) ? (PATH_MAX - 1) : newlen;
     _len = newlen;
     _str[_len] = '\0';
-    for (_basestr = _str + _len; _basestr != _str && *_basestr != '/'; --_basestr) { }
+    set_basestr();
+};
+
+inline void PathBuffer::set_basestr() {
+    for (_basestr = _str + _len; _basestr != _str && *_basestr != '/'; --_basestr) {}
+    ++_basestr;
 }
 
-bool PathBuffer::is_dir() const {
+inline bool PathBuffer::is_dir() const {
     if (!lstat())
         return false;
     return S_ISDIR(_stat.st_mode);
 }
 
-FileTreeIterator::FileTreeIterator(const char* dirname, CallBack func) : _func(func){
-    if (!_path.add_file(dirname))
-        exit(1);
-}
